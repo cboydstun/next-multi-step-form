56995261d9fd130d6ef189a55c1e2702ca882858
cboydstun <cboydstun@users.noreply.github.com>
2019-10-16 14:51:01 -0500

    render userForm

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class FormUserDetails extends Component {
+    render () {
+        return (
+            <div>
+                <h1> hello from formUserDetails</h1>
+
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails;
