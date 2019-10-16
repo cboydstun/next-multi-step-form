daae181f926a08f9046bd7baa8577763be24b0c9
cboydstun <cboydstun@users.noreply.github.com>
2019-10-16 15:00:22 -0500

    import component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { UserForm } from './components/UserForm';
